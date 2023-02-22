861f8c2f7a2b6bc23cc64e36643380c2641ab25f
araf821 <araf.ahmed200@gmail.com>
2023-02-22 18:40:47 -0500

    implemented nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
-export default Navbar
+    <nav className="navbar">
+      <section className="nav-center">
+        <Link to="/">
+          {/* Placeholder title for now, will be replacing with img */}
+          <h3>Cocktails</h3>
+        </Link>
+
+        <ul className="nav-links">
+          <li>
+            <Link to="/" className="link-hover">
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link to="about" className="link-hover">
+              About
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </nav>
+  );
+};
+export default Navbar;
