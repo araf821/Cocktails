acf4ce524838c74d18f57cc67fbe4c7012f261b1
araf821 <araf.ahmed200@gmail.com>
2023-02-22 17:03:24 -0500

    set up all the different routes in our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
 import SharedLayout from "./pages/SharedLayout";
+import SingleCocktail from "./pages/SingleCocktail";
 
 function App() {
   return (
     <BrowserRouter className="App">
       <Routes>
+        
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
+
+        <Route path="cocktail" element={<SharedLayout />}>
+          <Route path=":cocktailId" element={<SingleCocktail />} />
+        </Route>
+
+        <Route path="*" element={<Error />} />
+
       </Routes>
     </BrowserRouter>
   );
