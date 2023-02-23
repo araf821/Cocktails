c5828397bb7d48dd31924f76c16fdfdcd6931e60
araf821 <araf.ahmed200@gmail.com>
2023-02-22 20:31:35 -0500

    displayed list of drinks

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,18 @@
-const Card = () => {
+import { Link } from "react-router-dom";
+
+const Card = ({ id, name, category, img, alcoholic }) => {
   return (
-    <div>Card</div>
-  )
-}
-export default Card
+    <article className="card">
+      <div className="img-container">
+        <img src={img} alt={name} />
+      </div>
+      <div className="card-footer">
+        <h3>{name}</h3>
+        <h4>{category}</h4>
+        <p>{alcoholic}</p>
+        <Link to={`/cocktail/${id}`} className="cardBtn" >Pick Me</Link>
+      </div>
+    </article>
+  );
+};
+export default Card;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,36 @@
 import { useEffect } from "react";
 import { useGlobalContext } from "../context";
+import Card from "./Card";
 
 const List = () => {
   const { data } = useGlobalContext();
 
-  return <div>List</div>;
+  return (
+    <main className="cocktails-section">
+      <h1 className="list-title">Cocktails</h1>
+      <section className="list-section">
+        {data.map((cocktail) => {
+          // Destructure and rename a few different fields.
+          const {
+            idDrink,
+            strDrink,
+            strCategory,
+            strDrinkThumb,
+            strAlcoholic,
+          } = cocktail;
+          return (
+            <Card
+              key={idDrink}
+              id={idDrink}
+              name={strDrink}
+              category={strCategory}
+              img={strDrinkThumb}
+              alcoholic={strAlcoholic}
+            />
+          );
+        })}
+      </section>
+    </main>
+  );
 };
 export default List;
