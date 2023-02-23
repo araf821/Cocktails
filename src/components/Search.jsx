c64f6c280765577c318f8d781d9312c50330c356
araf821 <araf.ahmed200@gmail.com>
2023-02-22 19:42:03 -0500

    implemented search form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,26 @@
+import { useRef } from "react";
+import { useGlobalContext } from "../context";
+
 const Search = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+
   return (
-    <div>Search</div>
-  )
-}
-export default Search
+    <section className="search-box">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className="form-control">
+          <label htmlFor="name">Search For Your Favourite Cocktail</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            ref={searchValue}
+            placeholder="Pina Colada"
+            onChange={() => setSearchTerm(searchValue.current.value)}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+export default Search;
